9e551e6aafb9f597fe846c7c8bb351e94dd16e5b
JuanTellez125 <juanestellez125@gmail.com>
2026-03-27 11:47:20 -0500

    feat: Se creo InvalidBookDataException y LoanAlreadyReturnedException

---
+++ src/main/java/edu/eci/dosw/tdd/core/exception/InvalidBookDataException.java
@@ -0,0 +1,7 @@
+package edu.eci.dosw.tdd.core.exception;
+
+public class InvalidBookDataException extends RuntimeException {
+    public InvalidBookDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/edu/eci/dosw/tdd/core/exception/LoanAlreadyReturnedException.java
@@ -0,0 +1,7 @@
+package edu.eci.dosw.tdd.core.exception;
+
+public class LoanAlreadyReturnedException extends RuntimeException {
+    public LoanAlreadyReturnedException(String message) {
+        super(message);
+    }
+}
